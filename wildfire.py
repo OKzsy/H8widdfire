2f41ad80be4baf48289542d5409e4e6df08de5b3
ZSS\zss <hpu_zss@163.com>
2020-03-25 17:50:22 +0800

    实现云掩模模块编写，缺少裁剪模块

--- wildfire.py
+++ wildfire.py
@@ -61,7 +61,7 @@ def getsubsetdata(datasets, subdataset_name):
 
 def transformTogeotiff(file, outdir):
     visual_band_list = ['albedo_01', 'albedo_02', 'albedo_03', 'albedo_04', 'albedo_05']
-    nir_band_list = ['tbb_07', 'tbb_14']
+    nir_band_list = ['tbb_07', 'tbb_14', 'tbb_15']
     # 获取nc数据的文件名
     basename = os.path.splitext(os.path.basename(file))[0]
     # 创建可见光影像文件名
@@ -119,42 +119,7 @@ def transformTogeotiff(file, outdir):
     return visual_file, nir_file
 
 
-def Extend(xs, ys, matrix):
-    """
-    根据滤波模板的大小，对原始影像矩阵进行外扩。
-    :param xs: 滤波模板的xsize，要求为奇数
-    :param ys: 滤波模板的ysize，要求为奇数
-    :param matrix: 原始影像矩阵
-    :return: 依据模板大小扩展后的矩阵
-    """
-    xs_fill = int((xs - 1) / 2)
-    ys_fill = int((ys - 1) / 2)
-    # 使用镜像填充
-    extended_val = np.pad(matrix, ((ys_fill, ys_fill), (xs_fill, xs_fill)), "reflect")
-    matrix = None
-    return extended_val
-
-
-def img_filtering(xs, ys, ori_xsize, ori_ysize, kernel, ext_img):
-    """
-
-    :param xs: 卷积核大小：列
-    :param ys: 卷积核大小：行
-    :param kernel: 卷积核
-    :param ext_img: 经扩展后的影像
-    :return: 滤波后的影像
-    """
-    # 使用切片后影像的波段书
-    # 创建切片后存储矩阵
-    # 使用滑动窗口
-    filtered_img = np.zeros((math.ceil(ori_ysize / ys), math.ceil(ori_xsize / xs)), dtype=np.float16)
-    for irow in range(ys):
-        for icol in range(xs):
-            filtered_img += ext_img[irow: irow + ori_ysize: ys, icol: icol + ori_xsize: xs] * kernel[irow, icol]
-    return filtered_img
-
-
-def generat_mask(vis_file_path):
+def generat_mask(vis_file_path, nir_file_path):
     """利用可见光近红外波段数据生成云，水，冰雪掩模"""
     vis_ds = gdal.Open(vis_file_path)
     xsize = vis_ds.RasterXSize
@@ -162,77 +127,48 @@ def generat_mask(vis_file_path):
     geos = vis_ds.GetGeoTransform()
     prj = vis_ds.GetProjection()
     bands_data = vis_ds.ReadAsArray()
-    # 创建掩模矩阵
-    mask = np.ones((ysize, xsize), dtype=np.byte)
-    # # 计算冰雪掩模
-    # ndsi = (bands_data[0, :, :] - bands_data[4, :, :]) / (bands_data[0, :, :] + bands_data[4, :, :])
-    # ndsi_index = np.where(ndsi > 0.13)
-    # mask[ndsi_index] = 0
-    # ndsi = None
-    # ndsi_index = None
-    # # 计算水体掩模
-    # ndwi = (bands_data[3, :, :] - bands_data[2, :, :]) / (bands_data[3, :, :] + bands_data[2, :, :])
-    # ndwi_index = np.where(ndwi < 0)
-    # mask[ndwi_index] = 0
-    # ndwi = None
-    # ndwi_index = None
+    # 计算冰雪掩模
+    ndsi = (bands_data[0, :, :] - bands_data[4, :, :]) / (bands_data[0, :, :] + bands_data[4, :, :])
+    mask = ndsi > 0.13
+    ndsi = None
+    # 计算水体掩模
+    ndwi = (bands_data[3, :, :] - bands_data[2, :, :]) / (bands_data[3, :, :] + bands_data[2, :, :])
+    mask = np.bitwise_or(ndwi < 0, mask)
+    ndwi = None
     # 计算云掩模
-    # 获取0.51微米波段反射率
-    ref_green = bands_data[1, :, :] * 0.0001
-    # ref_green = np.arange(9).reshape(3, 3) + 1
-    # 考虑边缘像素，首先对影像边缘进行处理
-    win_xs = 3
-    win_ys = 3
-    # xsize = 3
-    # ysize = 3
-    ext_ref_green = Extend(win_xs, win_ys, ref_green)
-    kernel = np.ones((win_ys, win_xs)) / 9
-    part1 = img_filtering(win_xs, win_ys, xsize, ysize, kernel, ext_ref_green * ext_ref_green)
-    part2 = img_filtering(win_xs, win_ys, xsize, ysize, kernel, ext_ref_green)
-    part2 **= 2
-    variance = np.maximum(part1 - part2, 0)
-    ref_green_std = np.sqrt(variance)
-    ref_green_std_index = np.where(ref_green_std > 0.01)
-    tmp_mask = np.ones_like(ref_green_std, dtype=np.byte)
-    tmp_mask[ref_green_std_index] = 0
-    # 对原始掩模进行处理
-    ext_mask = Extend(win_xs, win_ys, mask)
-    for irow in range(win_ys):
-        for icol in range(win_xs):
-            ext_mask[irow: irow + ysize: win_ys, icol: icol + xsize: win_xs] = \
-                np.bitwise_and(ext_mask[irow: irow + ysize: win_ys, icol: icol + xsize: win_xs], tmp_mask)
-    mask = ext_mask[win_ys // 2: -(win_ys // 2), win_xs // 2: -(win_xs // 2)]
-    # ref_green = ext_ref_green = part1 = part2 = ref_green_std = ref_green_std_index = None
-    # # 获取0.47微米波段反射率
-    # ref_blue = bands_data[0, :, :] * 0.0001
-    # ref_blue_index = np.where(ref_blue > 0.25)
-    # mask[ref_blue_index] = 0
-    # ref_blue = ref_blue_index = None
-    # 临时写出掩模
+    # 读取亮温数据
+    BT_ds = gdal.Open(nir_file_path)
+    bt_data = BT_ds.ReadAsArray()
+    part1 = np.bitwise_or(bt_data[1, :, :] < 270, bt_data[2, :, :] < 265)
+    part2 = np.bitwise_and(bt_data[1, :, :] < 280, (bt_data[0, :, :] - bt_data[1, :, :]) > 20)
+    part3 = np.bitwise_and(bt_data[1, :, :] < 280, (bt_data[1, :, :] - bt_data[2, :, :]) < 3)
+    cld_mask = np.bitwise_or(np.bitwise_or(part1, part2), part3)
+    part1 = part2 = part3 = None
+    bt_data = BT_ds = None
+    mask = np.bitwise_or(cld_mask, mask).astype(np.byte) * 255
+    # 写出掩模
     basename = os.path.splitext(os.path.basename(vis_file_path))[0]
     tmp_dir = r"F:\kuihua8\out"
     mask_path = os.path.join(tmp_dir, basename) + "_cld.tif"
     tif_driver = gdal.GetDriverByName('GTiff')
     out_ds = tif_driver.Create(mask_path, xsize, ysize, 1, gdal.GDT_Byte)
-    # out_ds = tif_driver.Create(mask_path, xsize, ysize, 1, gdal.GDT_Float32)
     out_ds.SetGeoTransform(geos)
     out_ds.SetProjection(prj)
     out_ds.GetRasterBand(1).WriteArray(mask)
-    # out_ds.GetRasterBand(1).WriteArray(ref_green_std)
     out_ds = None
     return 1
 
 
 def main(indir, outdir, shp_file=None):
-    # search files
-    files = searchfiles(indir, partfileinfo='*20200315_0300*.nc')
+    # 搜索文件
+    files = searchfiles(indir, partfileinfo='*.nc')
     for ifile in files:
         visual_file, nir_file = transformTogeotiff(ifile, outdir)
         # 预留裁剪模块，只处理感兴趣区域
         if shp_file is not None:
             pass
         # 生成云，水，冰雪掩模
-        cld = generat_mask(visual_file)
+        cld = generat_mask(visual_file, nir_file)
     return None
 
 
